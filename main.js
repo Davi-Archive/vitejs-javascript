0114d8d4b4fce3ef09f7db3df758d54336bc2123
davi38 <davi4alves@gmail.com>
2022-08-22 23:34:41 -0300

    all study methods

--- main.js
+++ main.js
@@ -1,23 +1,150 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+// Nullish Coalecent  Operator
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/* const idade = 0;  */       //null, 0, [], undefined
 
-setupCounter(document.querySelector('#counter'))
+/* document.body.innerText = 'Sua idade é: '+(idade || 'Não Informado') */
+
+//Objeto metodos
+
+/* const user = {
+  name: 'Davi',
+  idade: 28,
+  address: {
+    street: 'Rua Teste',
+    number: 176,
+  },
+};
+ */
+///REST OPERATOR
+
+/* const {address, idade: age, nickname} = user; */
+
+/* const {name, ...rest} = user;    */         //rest Operator
+
+/* const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const [first, , third, ...rest] = array;
+document.body.innerText = JSON.stringify({first, third, rest}) */
+
+
+/// Short Syntax
+/*
+const name = 'Davi';
+const idade = 28;
+
+const user = {
+  name,
+  idade,
+}
+document.body.innerText = JSON.stringify({user})
+ */
+
+/* const user = {
+  name: 'Davi',
+  idade: 28,
+  address: {
+    street: 'Rua Teste',
+    number: 176,
+    zip: {
+      code: '088093813a4',
+      city: 'Rio do Norte',
+    },
+    showFullAddress(){
+      return 'ok';
+    }
+  },
+}; */
+/*
+const key = 'name'; */
+/* const key = 'name'; */
+
+/* document.body.innerText = user.address.showFullAddress?.() ?? 'Não Informado' */
+/* document.body.innerText = user[key]; */
+
+
+
+const array = [1, 2 ,3 ,4, 5];
+
+
+/* for (const i of array){
+  document.body.innerText += i;
+} */
+
+/* array.forEach(item => {
+  document.body.innerText += item;
+})
+ */
+
+/* const novoArray = array.map(item =>{
+  if (item %2 === 0){
+    return item * 5
+  }
+  return item;
+}) */
+
+
+/* let novoArray = array
+.filter(item => item % 2 === 0)
+.map(item => item * 10);   //20,40 */
+
+/* const soma = array.reduce((acc, item)=> {
+  return acc + item;
+}, 0)
+
+const name = 'Davi';
+const message = `Bem-vindo, ${name}`
+
+
+document.body.innerText = message; */
+
+
+// PROMISE
+
+/* const somaDoisNumeros = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve(a + b);
+    }, 2000)
+  })
+}
+
+somaDoisNumeros(1, 5)
+.then(soma => {
+  document.body.innerText = soma
+})
+.catch(err=> {
+  console.log(err);
+}) */
+
+
+
+/* fetch('https://api.github.com/users/davi38')
+.then(response => {
+  return console.log(response.json());
+})
+.catch(err =>{
+  console.log(err);
+})
+.finally(()=>{
+  console.log('deu');
+}) */
+
+//// PROMISE
+
+
+async function buscaDadosNoGithub() {
+  try {
+    const response = await fetch('https://api.github.com/users/davi38');
+    const body = await response.json();
+    return body.name;
+  }
+  catch (err) {
+    console.log(err);
+  }
+  finally {
+    console.log('deu');
+  }
+}
+
+buscaDadosNoGithub().then(name => {
+  console.log(name)
+})
